acf75ea6bd6823cdbff0c23d95263b49cc4d5cd8
Jaroslaw Kotus <jaroslawkotus@gmail.com>
2019-04-17 15:43:08 +0200

    changes after pull request

--- config/test-config.ts
+++ config/test-config.ts
@@ -1,4 +1,4 @@
 export const testConfig = {
     baseUrl: process.env.baseUrl || "http://jacekokrojek.github.io/jak-to-zrobic-w-js",
-    messageFromContactPage: "Your message has been sent.",
+    messageFromContactPage: "Your message has been sent."
 }
