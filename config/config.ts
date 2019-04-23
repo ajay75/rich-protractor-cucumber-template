3933c512f9cf955fb97c55780a9fb73c54ba0b32
Jaroslaw Kotus <jaroslawkotus@gmail.com>
2019-04-23 09:11:18 +0200

    retrun changes in config file

--- config/config.ts
+++ config/config.ts
@@ -9,11 +9,12 @@ export const config: Config = {
     SELENIUM_PROMISE_MANAGER: false,
 
     capabilities: {
-        shardTestFiles: true,
+        shardTestFiles: false,
         maxInstances: 1,
         browserName: "chrome",
+        restartBrowserBetweenTests: false,
         chromeOptions: {
-            args: ['--no-sandbox', '--disable-setuid-sandbox', '--test-type=browser', '--enable-logging', '--v=1'],
+            args: ['--no-sandbox', '--test-type=browser'],
             prefs: {
                 'plugins.always_open_pdf_externally': true,
                 'download': {
@@ -25,7 +26,6 @@ export const config: Config = {
             },
         },
     },
-
 
     framework: "custom",
     frameworkPath: require.resolve("protractor-cucumber-framework"),
